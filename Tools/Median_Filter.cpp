d41e21f41aec3939ab6982116aec19d40d1a0204
Michael du Preez <michaeljohn.dupreez@gmail.com>
2020-03-18 12:01:14 +0200

    Editing median_filter_func parameters to include array dimensions as I am having trouble calculating dimensions of parsed array

--- Tools/Median_Filter.cpp
+++ Tools/Median_Filter.cpp
@@ -1,19 +1,19 @@
 // EEE4120F - Prac 3 - Median_Filter.cpp
 // DPRMIC007 & BRDCAL003
 
-int[][] median_filter_func (int input[][], int windowSizeX, int windowSizeY, int boundaryCond)
+int[][] median_filter_func (int input[][],int xSize, int ySize, int windowSizeX, int windowSizeY, int boundaryCond)
 {
   // boundaryCond: 0 = ignore(without cropping), 1 = ignore(with cropping). 2 = wrap, 3 = shrink window
 
   // Setting up output array
   if(boundaryCond == 1)
   {
-    inputSizeX = (sizeof(input) / sizeof(input[0])) - windowSizeX;
-    inputSizeY = (sizeof(input[0]) / sizeof(input[0][0])) - windowSizeY;
+    inputSizeX = xSize - windowSizeX;
+    inputSizeY = ySize- windowSizeY;
   } else
   {
-    inputSizeX = sizeof(input) / sizeof(input[0]);
-    inputSizeY = sizeof(input[0]) / sizeof(input[0][0]);
+    inputSizeX = xSize;
+    inputSizeY = ySize;
   }
 
   int output [inputSizeX][inputSizeY];
@@ -34,7 +34,7 @@ int[][] median_filter_func (int input[][], int windowSizeX, int windowSizeY, int
             counter ++ ;
           }
         }
-
+        //input[y][x] =
     }
   }
 
