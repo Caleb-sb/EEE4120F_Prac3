520c6355a6dcc4d54daa2f642b8ccceaa9be42e9
Michael du Preez <michaeljohn.dupreez@gmail.com>
2020-03-18 10:27:06 +0200

    Declaring Function

--- Tools/Median_Filter.cpp
+++ Tools/Median_Filter.cpp
@@ -1,4 +1,8 @@
 // EEE4120F - Prac 3 - Median_Filter.cpp
 // DPRMIC007 & BRDCAL003
 
-median_filter_func(int [][])
+int [][] median_filter_func(int input[][], int windowSizeX, int windowSizeY, int boundaryCond)
+{
+  // boundaryCond: 0 = ignore, 1 = wrap, 2 = shrink window
+
+}
