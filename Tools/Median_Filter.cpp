8bea324b407caa8cc28afbb0b28b80a1233a8d40
Michael du Preez <michaeljohn.dupreez@gmail.com>
2020-03-18 11:13:32 +0200

    Setting up for loops and creating temporary list

--- Tools/Median_Filter.cpp
+++ Tools/Median_Filter.cpp
@@ -1,7 +1,7 @@
 // EEE4120F - Prac 3 - Median_Filter.cpp
 // DPRMIC007 & BRDCAL003
 
-int [][] median_filter_func(int input[][], int windowSizeX, int windowSizeY, int boundaryCond)
+int[][] median_filter_func (int input[][], int windowSizeX, int windowSizeY, int boundaryCond)
 {
   // boundaryCond: 0 = ignore(without cropping), 1 = ignore(with cropping). 2 = wrap, 3 = shrink window
 
@@ -19,7 +19,24 @@ int [][] median_filter_func(int input[][], int windowSizeX, int windowSizeY, int
   int output [inputSizeX][inputSizeY];
 
 
+  for(int y = 0; y < inputSizeY; y ++)
+  {
+    for(int x = 0; x < inputSizeX; x ++)
+    {
+      int tempList[windowSizeY*windowSizeX]; // temp list to hold matrix items
+      int counter = 0; // counter used to itterate through tempList
+      // now add matrix items to list:
+        for(int insideY = 0; insideY < windowSizeY; insideY ++)
+        {
+          for(int insideX = 0; insideX < windowSizeX; insideX ++)
+          {
+            tempList[counter] = input[y + insideY][x + insideX];
+            counter ++ ;
+          }
+        }
 
+    }
+  }
 
 
 }
