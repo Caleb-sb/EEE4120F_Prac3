aae849e9c7e03a07d7b8a8aac26ec2252c4fae02
Michael du Preez <michaeljohn.dupreez@gmail.com>
2020-03-18 10:48:48 +0200

    Editting output array setup & boundaryCond variable

--- Tools/Median_Filter.cpp
+++ Tools/Median_Filter.cpp
@@ -3,12 +3,23 @@
 
 int [][] median_filter_func(int input[][], int windowSizeX, int windowSizeY, int boundaryCond)
 {
-  // boundaryCond: 0 = ignore, 1 = wrap, 2 = shrink window
+  // boundaryCond: 0 = ignore(without cropping), 1 = ignore(with cropping). 2 = wrap, 3 = shrink window
 
   // Setting up output array
-  inputSizeX = sizeof(input) / sizeof(input[0]);
-  inputSizeY = sizeof(input[0]) / sizeof(input[0][0]);
+  if(boundaryCond == 1)
+  {
+    inputSizeX = (sizeof(input) / sizeof(input[0])) - windowSizeX;
+    inputSizeY = (sizeof(input[0]) / sizeof(input[0][0])) - windowSizeY;
+  } else
+  {
+    inputSizeX = sizeof(input) / sizeof(input[0]);
+    inputSizeY = sizeof(input[0]) / sizeof(input[0][0]);
+  }
+
   int output [inputSizeX][inputSizeY];
+
+
+
 
 
 }
