6db80e2cf73c38c306bfb88b5b516fd1162c17da
Michael du Preez <michaeljohn.dupreez@gmail.com>
2020-03-18 10:40:27 +0200

    Setting up output array

--- Tools/Median_Filter.cpp
+++ Tools/Median_Filter.cpp
@@ -5,4 +5,10 @@ int [][] median_filter_func(int input[][], int windowSizeX, int windowSizeY, int
 {
   // boundaryCond: 0 = ignore, 1 = wrap, 2 = shrink window
 
+  // Setting up output array
+  inputSizeX = sizeof(input) / sizeof(input[0]);
+  inputSizeY = sizeof(input[0]) / sizeof(input[0][0]);
+  int output [inputSizeX][inputSizeY];
+
+
 }
